7e6f802b6798ca84fb8a20249b49e0d31276219b
Dan Magro <dando385@yahoo.com>
2025-06-11 09:57:33 -0400

    asteroids project complete

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 
 class Asteroid(CircleShape):
@@ -11,3 +13,27 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+    # 1. Kill this asteroid
+        self.kill()
+
+    # 2. If it was already the smallest size, stop here
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+    # 3. Compute split parameters
+    # a random angle between 20° and 50°
+        angle = random.uniform(20, 50)
+    # two new velocity vectors, rotated off the original
+        v1 = self.velocity.rotate(angle) * 1.2
+        v2 = self.velocity.rotate(-angle) * 1.2
+    # new, smaller radius
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+    # 4. Spawn two new asteroids at this position
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a1.velocity = v1
+
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2.velocity = v2
