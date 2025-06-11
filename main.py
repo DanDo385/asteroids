4f788d50fce375bea8e55483833ea7652a57c53b
Dan Magro <dando385@yahoo.com>
2025-06-10 20:57:09 -0400

    added collides_with function

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()  # New clock object for FPS limiting
-    
-    # 1. Create our groups
-    updatables = pygame.sprite.Group()
-    drawables  = pygame.sprite.Group()
-    
-    # 2. Tell Player to auto-add itself to both groups
-    Player.containers = (updatables, drawables)
+    clock = pygame.time.Clock()
 
-    # 3. Instantiate after setting containers
-    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
-        dt = clock.tick(60) / 1000
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        # 4. Update everything in one call
-        updatables.update(dt)
 
-        screen.fill((0, 0, 0))
-       
-         # 5. Draw each sprite
-        for sprite in drawables:
-            sprite.draw(screen)
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
-        
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
-
